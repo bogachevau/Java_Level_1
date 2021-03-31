7e0632e95455b5ca25bdafad889df2bdb93839a9
Alexandr <alexandr.bogachev@gmail.com>
2021-03-31 20:26:32 +0300

    Сделал,вроде даже правильно работает всё кроме счетчиков

---
+++ src/main/java/hw6/Cat.java
@@ -0,0 +1,41 @@
+package hw6;
+
+public class Cat extends Animal{
+
+    private String name;
+    private final int MAX_RUN_LENGTH = 200;
+
+    public String getName() {
+
+        return name;
+    }
+    public Cat(String name){
+        this.name = name;
+    }
+    public int catCount = 0;
+    public Cat(){
+        catCount++;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run(int length) {
+        if (length >= 0 && length <= MAX_RUN_LENGTH){
+            System.out.println(name + " пробежал: " + length + " м.");
+        } else{
+            System.out.println("Введены некорректные данные\n" + "Введите число от 0 до " + MAX_RUN_LENGTH);
+        }
+    }
+
+    public int getMAX_RUN_LENGTH() {
+        return MAX_RUN_LENGTH;
+    }
+
+    @Override
+    public void swim(int length) {
+        System.out.println("Кот не умеет плавать");
+    }
+}
---
+++ src/main/java/hw6/Test.java
@@ -0,0 +1,24 @@
+package hw6;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Cat barsik = new Cat("Барсик");
+        barsik.run(200);
+        barsik.swim(5);
+        Dog sharik = new Dog("Шарик");
+        sharik.run(360);
+        sharik.swim(9);
+        Cat antosik = new Cat("Антосик");
+        antosik.run(199);
+        Dog lobzik = new Dog("Лобзик");
+        lobzik.run(250);
+        lobzik.swim(10);
+        Animal animal = new Animal();
+        System.out.println("Создано:\n"
+                + " - Собак: " + lobzik.dogCount + "\n"
+                + " - Котов: " + antosik.catCount + "\n"
+                + " - Животных: " + animal.animalCount);
+
+    }
+}
